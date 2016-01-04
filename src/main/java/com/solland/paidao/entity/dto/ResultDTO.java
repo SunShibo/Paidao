3de4824e216c7347c1512d72bd9e8567ccf421f7
孙诗博.信息技术中心.EC开发部 <sunshibo@yolo24.com>
2016-01-04 10:23:52 +0800

    add resultDTO class

---
+++ src/main/java/com/solland/paidao/entity/dto/ResultDTO.java
@@ -0,0 +1,64 @@
+package com.solland.paidao.entity.dto;
+
+import java.io.Serializable;
+
+/**
+ * @author Shibo Sun
+ * 2016/1/4.
+ */
+public class ResultDTO<T> implements Serializable {
+    public static final String ERRCODE_NONE = "0";
+    public static final String ERRCODE_CONSTRAINT_VIOLATION = "-1";
+    public static final String ERRCODE_PROCESSING = "-2";
+    public static final String ERRCODE_UNKOWN = "-9";
+    private boolean success = true;
+    protected String errCode = "0";
+    protected String errMsg;
+    protected T data;
+
+    public ResultDTO() {
+    }
+
+    public ResultDTO(T data) {
+        this.data = data ;
+    }
+
+    public ResultDTO (boolean success ,String errCode , String errMsg) {
+        this.success = success ;
+        this.errCode = errCode ;
+        this.errMsg = errMsg ;
+    }
+
+    public boolean isSuccess() {
+        return this.success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public T getData() {
+        return this.data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public String getErrCode() {
+        return this.errCode;
+    }
+
+    public void setErrCode(String errCode) {
+        this.errCode = errCode;
+    }
+
+    public String getErrMsg() {
+        return this.errMsg;
+    }
+
+    public void setErrMsg(String errMsg) {
+        this.errMsg = errMsg;
+    }
+}
+
